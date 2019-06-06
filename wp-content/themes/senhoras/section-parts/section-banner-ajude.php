405e8dd07e61a10992d5dd98d15da7f235748b9e
Felipe <felipe@code6.com.br>
2019-06-06 20:37:03 -0300

    add plugins de cache, segunça e performance

--- wp-content/themes/senhoras/section-parts/section-banner-ajude.php
+++ wp-content/themes/senhoras/section-parts/section-banner-ajude.php
@@ -1,11 +1,11 @@
 <section id="banner_ajude">
 	<div class="container" class="d-flex">
 		<div class="row">
-			<div class="col-lg-6 col-sm-12  wow slideInUp">
+			<div class="col-lg-6 col-sm-12  wow bounceInLeft">
 				<h3>Ajude-nos a <span class="text-azul">Ajudar</span> <span class="descricao">Venha apoiar a comunidade Vila Aparecida</span></h3>
 			</div>
-			<div class="col-lg-6 col-sm-12 text-center  wow slideInUp d-flex justify-content-center align-items-center">
-				<a class="text-azul btn btn-light d-flex align-items-center" href="#"><i class="icon-btn-ajuda"></i> Veja como ajudar!</a>
+			<div class="col-lg-6 col-sm-12 text-center wow bounceInRight d-flex justify-content-center align-items-center">
+				<a class="text-azul btn btn-light d-flex align-items-center" href="/como-ajudar"><i class="icon-btn-ajuda"></i> Veja como ajudar!</a>
 			</div>
 		</div>
 	</div>
